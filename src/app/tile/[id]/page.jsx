9305c7ec02c2f7b1a8ae43f45f9a5eb52e6a7b0b
shanto2575 <shanto2575@gmail.com>
2026-05-01 15:01:20 +0600

    small change and add button in details page

--- src/app/tile/[id]/page.jsx
+++ src/app/tile/[id]/page.jsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import { getTiles } from '../../../lib/api/data';
 import Image from 'next/image';
+import { Button } from '@heroui/react';
+import { FaArrowRight } from 'react-icons/fa';
 
 export async function generateMetadata({ params }) {
     const {id}=await params;
@@ -29,7 +32,7 @@ const TilesDetailsPage = async ({ params }) => {
     const { title, description, image, category, price, dimensions, material, currency, inStock } = tilesDetails;
     return (
         <div>
-            <div className='border shadow-[0_0_25px_rgba(0,0,0,0.15)] rounded-2xl m-12'>
+            <div className='border shadow-[0_0_25px_rgba(0,0,0,0.15)] rounded-2xl m-4 lg:m-12'>
                 <div className='flex flex-col lg:flex-row gap-10 p-5 border'>
                     <div className='relative  lg:w-[700px] lg:h-[700px] aspect-square'>
                         <Image
@@ -41,13 +44,17 @@ const TilesDetailsPage = async ({ params }) => {
                         />
                     </div>
                     <div className='space-y-5'>
-                        <h2 className='text-4xl font-bold'>Title : <span className='font-semibold text-green-600'>{title}</span></h2>
+                        <h2 className='text-2xl lg:text-4xl font-bold'>Title : <span className='font-semibold text-green-600'>{title}</span></h2>
                         <p className='text-xl font-bold'>Description : <span className='font-semibold'>{description}</span></p>
                         <p className='text-xl font-bold'>Category : <span className='font-semibold'>{category}</span></p>
                         <p className='text-xl font-bold'>Dimensions : <span className='font-semibold'>{dimensions}</span></p>
                         <p className='text-xl font-bold'>Material : <span className='font-semibold'>{material}</span></p>
                         <p className='text-xl font-bold'>Price : <span className='font-semibold'>{price} {currency}</span></p>
                         <p className='text-xl font-bold'>isStack : <span className='font-semibold'>{`${inStock === true ? 'Available' : 'Not Avilable'}`}</span></p>
+                        <div className=' flex items-center  gap-5'>
+                            <Link href={''}><Button className={'rounded'}>ADD To Cart</Button></Link>
+                            <Link href={'/all-tiles'}><Button className={'rounded'}>Back<FaArrowRight/></Button></Link>
+                        </div>
                     </div>
 
                 </div>
